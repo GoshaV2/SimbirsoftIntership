e137943ba54bd8f08ac04bc72867c528134ea887
Тиунов Георгий <34783tgs@mail.ru>
2023-03-13 20:34:17 +0300

    init project

---
+++ src/main/java/com/simbirsoft/intership/SimbirsoftIntershipApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.intership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirsoftIntershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirsoftIntershipApplication.class, args);
+    }
+
+}
